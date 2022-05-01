ac97546829a4a2cfa68fa8b6894d5c59f005e319
Rod Garland <rg6797@outlook.com>
2022-05-01 04:18:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useCallback } from 'react';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import logo from './logo.svg';
-import './App.css';
 import particlesOptions from "./particles.json";
 
 function App() {
@@ -11,33 +9,11 @@ function App() {
     }, [])
 
     return (
-        <div className="App">
-            <Particles options={particlesOptions} init={particlesInit}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
+        <div>
+            <Particles className=' fixed h-full w-full m-0 p-0 left-0 top-0 -z-10' options={particlesOptions} init={particlesInit}/>
+            <h1 className="text-3xl font-bold underline">
+                Coming Soon....
+            </h1>
         </div>
     );
 }
