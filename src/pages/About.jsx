f49c4ac835b4071431fb5dde60560d326f03dd6b
Rod Garland <rg6797@outlook.com>
2022-07-09 17:14:24 -0400

    About Page Start

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <>
-            <h1>About Me</h1>
-        </>
+        <div className='py-40 px-10'>
+            <h1 className=' text-white uppercase font-bold text-center pb-4 mx-0 my-auto text-4xl'>About <span className='text-[#ACE6E6]'>Me</span></h1>
+            <div className='greeting'>
+                <p className=' text-white text-justify text-xl leading-9'>Greetings! My name is Rod Garland hailing from South Georgia. I am very passionate 
+                    about Web Development and I love to use Web Technologies to building super cool products and designs. I just want to solve problems for different environments and niches using the power of technology.  
+                </p>
+                <p>Whether its for help, a project, job oppertunity, or even just to chat, feel free to reach out. I'd love to hear from you 
+                    
+                </p>
+            </div>
+        </div>
     );
 };
 
