98010f7a7fdec50c2e8cfe49b5830d915d4149c2
Rod Garland <rg6797@outlook.com>
2022-07-09 22:27:07 -0400

    Finished basic mobile about page

---
+++ src/components/WebSkillsItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {ReactComponent as HTMLIcon} from '../assets/skillsIcons/htmlIcon.svg'
+import {ReactComponent as CSSIcon} from '../assets/skillsIcons/cssIcon.svg'
+import {ReactComponent as JSIcon} from '../assets/skillsIcons/jsIcon.svg'
+import {ReactComponent as ReactIcon} from '../assets/skillsIcons/reactIcon.svg'
+import {ReactComponent as TSIcon} from '../assets/skillsIcons/tsIcon.svg'
+import {ReactComponent as NodeIcon} from '../assets/skillsIcons/nodeIcon.svg'
+ import {ReactComponent as GraphQLIcon} from '../assets/skillsIcons/graphqlIcon.svg'
+ import {ReactComponent as MongoIcon} from '../assets/skillsIcons/mongoIcon.svg'
+ import {ReactComponent as TailwindIcon} from '../assets/skillsIcons/tailwindIcon.svg'
+ import {ReactComponent as NextIcon} from '../assets/skillsIcons/nextjsIcon.svg'
+import {ReactComponent as VSCodeIcon} from '../assets/skillsIcons/vscodeIcon.svg'
+ import {ReactComponent as GithubIcon} from '../assets/skillsIcons/githubIcon.svg'
+ import {ReactComponent as VercelIcon} from '../assets/skillsIcons/vercelIcon.svg'
+
+const components = {
+    html: HTMLIcon,
+    css: CSSIcon,
+    javascript: JSIcon,
+    react: ReactIcon,
+    typescript: TSIcon,
+    node: NodeIcon,
+    graphql: GraphQLIcon,
+    mongo: MongoIcon,
+    tailwind: TailwindIcon,
+    nextjs: NextIcon,
+    vscode: VSCodeIcon,
+    github: GithubIcon,
+    vercel: VercelIcon
+}
+
+const WebSkillsItem = ({skillIcon}) => {
+    const SkillItem = components[skillIcon]
+    return (
+        <div className=' border-2 rounded-lg p-6 fill-white hover:fill-[#ACE6E6] '>
+            <SkillItem/>
+            <h2 className=' uppercase text-sm text-white text-center font-bold mt-1'>{skillIcon}</h2>
+        </div>
+    );
+};
+
+export default WebSkillsItem;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
-
+import WebSkillsItem from '../components/WebSkillsItem';
+import skillsData from '../skills.json'
 const About = () => {
     return (
-        <div className='py-40 px-10'>
-            <h1 className=' text-white uppercase font-bold text-center pb-4 mx-0 my-auto text-4xl'>About <span className='text-[#ACE6E6]'>Me</span></h1>
-            <div className='greeting'>
-                <p className=' text-white text-justify text-xl leading-9'>Greetings! My name is Rod Garland hailing from South Georgia. I am very passionate 
-                    about Web Development and I love to use Web Technologies to building super cool products and designs. I just want to solve problems for different environments and niches using the power of technology.  
-                </p>
-                <p>Whether its for help, a project, job oppertunity, or even just to chat, feel free to reach out. I'd love to hear from you 
-                    
-                </p>
+        <div className='py-40 px-10 flex flex-col space-y-12'>
+            <div className="about">
+                <h1 className=' text-white uppercase font-bold text-center pb-4 mx-0 my-auto text-4xl'>About <span className='text-[#ACE6E6]'>Me</span></h1>
+                <div className='greeting text-white text-justify text-xl leading-9'>
+                    <p className='pb-6'>Greetings! My name is Rod Garland hailing from South Georgia. I am very passionate 
+                        about Web Development and I love to use Web Technologies to building super cool products and designs. I just want to solve problems for different environments and niches using the power of technology.  
+                    </p>
+                    <p>Whether its for help, a project, job opportunity, or even just to chat, feel free to reach out. I'd love to hear from you  
+                    </p>
+                </div>
+            </div>
+            <div className="skills py-4">
+                <h1 className=' text-white uppercase font-bold text-center pb-4 mx-0 my-auto text-4xl'>My <span className='text-[#ACE6E6]'>Skills</span></h1>
+                <div className="skllList py-2 grid grid-cols-2 gap-10">
+                    {skillsData
+                        .filter((item) => !item.tool)
+                        .map((item) => (
+                            <WebSkillsItem skillIcon={item.tech}/>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="skills py-4">
+                <h1 className=' text-white uppercase font-bold text-center pb-4 mx-0 my-auto text-4xl'>Tool<span className='text-[#ACE6E6]'> I use</span></h1>
+                <div className="skllList py-2 grid grid-cols-2 gap-10">
+                    {skillsData
+                        .filter((item) => item.tool)
+                        .map((item) => (
+                            <WebSkillsItem skillIcon={item.tech}/>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
