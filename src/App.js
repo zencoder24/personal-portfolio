e58b82bd5bddfaaf43c8865bac9a129b3911485a
Rod Garland <rg6797@outlook.com>
2022-09-24 11:09:48 -0400

    Removed Blog component and all references

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React, { useCallback } from 'react';
+import React, { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import particlesOptions from "./particles.json";
-import { Routes, Route} from 'react-router-dom';
-import NavMenu from './components/NavMenu';
-import Navbar from './components/Navbar';
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Blog from './pages/Blog'
-import Contact from './pages/Contact'
-import Resume from './pages/Resume';
-
+import { Routes, Route } from "react-router-dom";
+import NavMenu from "./components/NavMenu";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
 function App() {
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
 
-    return (
-        <div>
-            <Particles className='fixed h-full w-full m-0 p-0 left-0 top-0 -z-10' options={particlesOptions} init={particlesInit}/>
-                <NavMenu/>
-                <Navbar/>
-                <Routes>
-                    <Route exact path='/' element={<Home/>}/>
-                    <Route path='/about' element={<About/>}/>
-                    <Route path='/resume' element={<Resume/>}/>
-                    <Route path='/projects' element={<Projects/>}/>
-                    <Route path='/blog' element={<Blog/>}/>
-                    <Route path='/contact' element={<Contact/>}/>
-                </Routes>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Particles
+        className="fixed h-full w-full m-0 p-0 left-0 top-0 -z-10"
+        options={particlesOptions}
+        init={particlesInit}
+      />
+      <NavMenu />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
