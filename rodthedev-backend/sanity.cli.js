fd2becfe53e624c4fbd8417aafac7acf5d5a069d
Rod Garland <rg6797@outlook.com>
2022-12-23 19:32:00 -0500

    Fixed file structure

--- rodthedev-backend/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'de2o2jud',
-    dataset: 'production'
-  }
-})
--- rodthedev-backend/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import blog from './blog'
-import author from './author'
-
-export const schemaTypes = [blog, author, category, blockContent]
