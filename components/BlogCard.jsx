b258bfc0354b25f4594aefb81e00ec2274587676
Rod Garland <rg6797@outlook.com>
2022-12-31 23:17:27 -0500

    Responsiveness to Blog Page

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -6,16 +6,20 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const BlogCard = () => {
   return (
-    <div className="max-w-sm bg-white ">
-      <a href="#">
+    <div className="max-w-sm  ">
+      <span class="bg-green-100 text-right my-2 float-right text-green-800 text-xs font-medium  px-2 py-1 rounded dark:bg-green-200 dark:text-green-900">
+        Supabase
+      </span>
+      <a href="#" className="">
         <Image src={mypic} alt="This is a test" className=" object-cover" />
       </a>
-      <div className="mt-2">
+      <div className="my-2">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-            Noteworthy technology acquisitions 2021
+          <h5 className=" text-2xl font-bold tracking-tight text-gray-900 ">
+            An article about Supabase
           </h5>
         </a>
+        <p className=" text-sm mb-2 text-gray-400">By: Rodderick Garland</p>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           Here are the biggest enterprise technology acquisitions of 2021 so
           far, in reverse chronological order.
--- pages/blogs.js
+++ pages/blogs.js
@@ -4,33 +4,35 @@ import BlogCard from "../components/BlogCard";
 export class Blogs extends Component {
   render() {
     return (
-      <div className="my-32 mx-6 ">
-        <div id="blogs-head" className="flex flex-col gap-6">
-          <h1 className="text-4xl font-bold">apex-blogs</h1>
-          <h3 className="text-xl">
-            A series of posts of concepts, best practices, and life lessons
-            learned while coding
-          </h3>
+      <div className="my-32 mx-6 md:mx-16 ">
+        <div id="blogs-head" className="flex flex-col gap-12 ">
+          <div className="flex flex-col gap-y-4 md:grid md:grid-cols-2">
+            <h1 className="text-4xl font-bold md:text-5xl">apex-blogs</h1>
+            <h3 className="text-xl md:text-right">
+              A series of posts of concepts, best practices, and life lessons
+              learned while coding.
+            </h3>
+          </div>
           <div>
-            <form className="flex flex-col gap-2">
+            <form className="flex flex-col gap-2 md:relative md:w-[30%]">
               <input
                 type="text"
                 placeholder="Enter your email"
-                className=" rounded-full py-2 border-2 border-emerald-400 placeholder:pl-4"
+                className=" rounded-full border py-3 pl-4 md:w-full"
               />
 
-              <button className="py-2 bg-black rounded-full text-white">
+              <button className="rounded-full bg-black py-3 text-white md:absolute md:top-0 md:right-0 md:px-4 md:font-semibold">
                 Subscribe
               </button>
             </form>
           </div>
 
-          <div className="text-xs mt-20 mb-6 font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-            <ul className="flex -mb-px">
+          <div className="mt-16 mb-6 border-b border-gray-200 text-center text-xs font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400 md:text-base">
+            <ul className="-mb-px flex">
               <li className="mr-1">
                 <a
                   href="#"
-                  className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                  className="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300"
                 >
                   All
                 </a>
@@ -38,7 +40,7 @@ export class Blogs extends Component {
               <li className="mr-1">
                 <a
                   href="#"
-                  className="inline-block p-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
+                  className="active inline-block rounded-t-lg border-b-2 border-blue-600 p-4 text-blue-600 dark:border-blue-500 dark:text-blue-500"
                   aria-current="page"
                 >
                   Next.js
@@ -47,7 +49,7 @@ export class Blogs extends Component {
               <li className="mr-1">
                 <a
                   href="#"
-                  className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                  className="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300"
                 >
                   Supabase
                 </a>
@@ -55,14 +57,18 @@ export class Blogs extends Component {
               <li className="mr-1">
                 <a
                   href="#"
-                  className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                  className="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300"
                 >
                   Code Life
                 </a>
               </li>
             </ul>
           </div>
-          <BlogCard />
+          <div className="flex flex-col gap-y-10 md:grid md:grid-cols-2 md:gap-10 lg:grid-cols-3">
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+          </div>
         </div>
       </div>
     );
